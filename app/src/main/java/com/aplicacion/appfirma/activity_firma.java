8db45effea38393c287c89f468b39ade7f966c87
Yeltsin Baquedano <yeltsinbaquedano@gmail.com>
2021-07-12 23:23:44 -0600

    Funcion para guardar

--- app/src/main/java/com/aplicacion/appfirma/activity_firma.java
+++ app/src/main/java/com/aplicacion/appfirma/activity_firma.java
@@ -2,11 +2,19 @@ package com.aplicacion.appfirma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+
+import java.io.ByteArrayOutputStream;
 
 public class activity_firma extends AppCompatActivity {
     private CaptureBitmapView mSig;
+    byte[] byteArray;
+    ImageView objImagen;
+    SQLiteConexion db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +24,13 @@ public class activity_firma extends AppCompatActivity {
         LinearLayout mContent = (LinearLayout) findViewById(R.id.signLayout);
         mSig = new CaptureBitmapView(this, null);
         mContent.addView(mSig, LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.FILL_PARENT);
+    }
+
+    private void getBytes(Intent data){
+        Bitmap photo = (Bitmap) data.getExtras().get("data");
+        objImagen.setImageBitmap(photo);
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        photo.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byteArray = stream.toByteArray();
     }
 }
