fa1988be0d1e94f5e2e739948d03d75705d0244a
Yeltsin Baquedano <yeltsinbaquedano@gmail.com>
2021-07-13 00:07:28 -0600

    test

--- app/src/main/java/com/aplicacion/appfirma/activity_firma.java
+++ app/src/main/java/com/aplicacion/appfirma/activity_firma.java
@@ -5,13 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import java.io.ByteArrayOutputStream;
 
 public class activity_firma extends AppCompatActivity {
-    private CaptureBitmapView mSig;
+    public CaptureBitmapView mSig;
     byte[] byteArray;
     ImageView objImagen;
     SQLiteConexion db;
@@ -24,10 +28,41 @@ public class activity_firma extends AppCompatActivity {
         LinearLayout mContent = (LinearLayout) findViewById(R.id.signLayout);
         mSig = new CaptureBitmapView(this, null);
         mContent.addView(mSig, LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.FILL_PARENT);
+
+        Button btnSalvar = (Button) findViewById(R.id.btn_salvar);
+        Button btnClean = (Button) findViewById(R.id.btn_limpiar);
+        EditText descripcion = (EditText) findViewById(R.id.editTextTextPersonName2);
+
+
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bitmap signature = mSig.getBitmap();
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                signature.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                byteArray = stream.toByteArray();
+
+                /*getBytes(mSig.getBitmap());*/
+                if(byteArray.length != 0) {
+                    db.insert(byteArray, "Hey");
+                    Toast.makeText(getApplicationContext(), "Guardado en la Base de Datos", Toast.LENGTH_LONG).show();
+                    byteArray = new byte[0];
+                    descripcion.setText("");
+                }else{
+                    Toast.makeText(getApplicationContext(), "No se ha tomado ninguna fotografia", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        btnClean.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSig.ClearCanvas();
+            }
+        });
     }
 
-    private void getBytes(Intent data){
-        Bitmap photo = (Bitmap) data.getExtras().get("data");
+    private void getBytes(Bitmap photo){
         objImagen.setImageBitmap(photo);
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
         photo.compress(Bitmap.CompressFormat.PNG, 100, stream);
